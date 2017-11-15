ebfda6c180f7689bebc64f7ace2b2936879d0ba5
Timothy Shores <timothymshores@gmail.com>
2017-11-14 19:29:24 -0600

    Created routes to display JSON for every individual product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,19 @@ class ApplicationController < ActionController::API
     render json: products.as_json
   end
 
+  def first_product_method
+    products = Product.first
+    render json: products.as_json
+  end
+
+  def second_product_method
+    products = Product.find_by(id: 2)
+    render json: products.as_json
+  end
+
+  def last_product_method
+    products = Product.last
+    render json: products.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/all_products" => "products#all_products_method"
+  get "/first_product" => "products#first_product_method"
+  get "/second_product" => "products#second_product_method"
+  get "/last_product" => "products#last_product_method"
 end
